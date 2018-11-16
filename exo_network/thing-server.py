b66a7ce9b82aae364560154f133b828359f0d58c
Reyoth <serialkiwi@gmail.com>
2018-11-16 09:33:12 +0100

    cours

--- exo_network/thing-server.py
+++ exo_network/thing-server.py
@@ -3,30 +3,9 @@
 import RPi.GPIO as GPIO
 import time
 import network
-import SpyTank
 
-SWITCH = 4
+SWITCH = #a definir
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SWITCH, GPIO.IN)
 
-def heard(phrase):
-  print "heard:" + phrase
-  for a in phrase:
-    if a == "\r" or a == "\n":
-      pass # strip it
-    else:
-      if (GPIO.input(SWITCH)):
-        network.say("1")
-      else:
-        network.say("0")
 
-while True:
-  print "waiting for connection"
-  network.wait(whenHearCall=heard)
-  print "connected"
-
-  while network.isConnected():
-    print "server is running"  
-    time.sleep(1)
-
-  print "connection closed"
