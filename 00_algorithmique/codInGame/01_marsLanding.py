566647f606e8694522a7269ff5ebf1047530d377
Reyoth <serialkiwi@gmail.com>
2019-03-01 12:58:58 +0100

    ajout algorithmique avec exo python, codingame et vocabulaire

---
+++ 00_algorithmique/codInGame/01_marsLanding.py
@@ -0,0 +1,4 @@
+# Pour atterrir en toute securite, notre vitesse de chutte (v_speed) doit etre plus petite que 40
+# Si on monte, la valeur de v_speed est positif
+# La vitesse des propulseur est comprise entre 0 et 4 qui veut dire entre 0 et 4m/s
+# la vitesse de la chutte si les propulseurs sont eteints est de 3,711m/s
