ab22945a0801af76e0f86c4b0614cfdc77417318
Reyoth <serialkiwi@gmail.com>
2019-03-09 17:55:30 +0100

    ajout network

--- 04_morse/morse_reseau/client.py
+++ 04_morse/morse_reseau/client.py
@@ -1,6 +1,9 @@
 # coding: utf-8 
 import network
+<<<<<<< HEAD
 import comMorse
+=======
+>>>>>>> ajout network
 
 
 ADDRESS=""
@@ -13,10 +16,16 @@ while True:
 
     print("message a envoyer:")
     message = input(">> ") # utilisez raw_input() pour les anciennes versions python
+<<<<<<< HEAD
     morse = comMorse.encodeTexte(message)
     sock.send(morse.encode())
 
     reponse = sock.recv(4096)
+=======
+    sock.send(message.encode())
+
+    reponse = sock.recv(9999999)
+>>>>>>> ajout network
 
     print(reponse.decode("utf-8"))
     if message =="exit":
--- 04_morse/morse_reseau/serveur.py
+++ 04_morse/morse_reseau/serveur.py
@@ -1,6 +1,9 @@
 # coding: utf-8 
 import network
+<<<<<<< HEAD
 import comMorse
+=======
+>>>>>>> ajout network
 
 ADDRESS=""
 PORT=1111
@@ -10,6 +13,7 @@ sock.bind((ADDRESS,PORT))
 
 def communication(thread):
     
+<<<<<<< HEAD
     message = thread.clientsocket.recv(4096)
     message = message.decode("utf-8")
 
@@ -20,6 +24,16 @@ def communication(thread):
     thread.clientsocket.send("serveur : j'ai bien recu le message".encode())
     
     if message=="exit":
+=======
+    reponse = thread.clientsocket.recv(2048)
+    reponse = reponse.decode("utf-8")
+
+    print("message de %s %s" % (thread.ip, thread.port, ))
+    print(">>", reponse)
+    thread.clientsocket.send("serveur : j'ai bien recu le message".encode())
+    
+    if reponse=="exit":
+>>>>>>> ajout network
         return False
     else:
         return True
@@ -30,7 +44,11 @@ continuer = True
 while continuer:
 
     sock.listen(10)
+<<<<<<< HEAD
     print( "En ecoute...")
+=======
+    print( "En écoute...")
+>>>>>>> ajout network
 
     thread = network.newThread(sock.accept())
     thread.start()
