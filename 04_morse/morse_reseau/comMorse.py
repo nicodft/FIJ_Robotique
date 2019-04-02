682fd0a25b3c9e9b8709666e7a4bd6be7b852aa8
Reyoth <serialkiwi@gmail.com>
2019-04-02 13:42:57 +0200

    modif morse et ajout commentaires

--- 04_morse/morse_reseau/comMorse.py
+++ 04_morse/morse_reseau/comMorse.py
@@ -77,11 +77,47 @@ lettres=[
     "8",
     "9"
 ]
-
+# Fon*ction prennant une lettre en parametre et renvoyant son equivalent en morse
 def encode(lettre="a"):
+    """Fonction prennant une lettre en parametre et renvoyant son equivalent en morse
+    sous forme de chaine de caractere"""
     index = lettres.index(lettre)
     return codesMorse[index]
 
 def decode(code="..."):
+    """Fonction prennant un code morse en parametre et renvoyant la lettre correspondante
+    sous forme de chaine de caractere"""
     index = codesMorse.index(code)
     return lettres[index]
+
+def encodeTexte(phrase):
+    """Fonction prennant une phrase ou un mot et renvoyant son equivalent en morse
+    sous forme de chaine de caractere.
+    Pour faciliter la lecture, des espaces sont inserer entre les codes.
+    Des / sont aussi inserer pour representer les espaces entre les mots"""
+    
+    reponse=""
+    for lettre in phrase:
+        if lettre != " " and lettre != "":
+            reponse= reponse + encode(lettre)
+            reponse= reponse + " "
+        else:
+            reponse= reponse + "/ "
+    return reponse
+
+
+
+def decodeMorse(morseBrut):
+    """Fonction prennant une phrase ou un mot  en morse et renvoyant son equivalent texte
+    sous forme de chaine de caractere."""
+    
+    reponse=""
+    morseBrut = str.split(morseBrut," ")
+    for element in morseBrut:
+            if element != "/" and element != "":
+                reponse= reponse + decode(element)
+            elif element == "/":
+                reponse= reponse + " "
+            else:
+                pass
+    return reponse
