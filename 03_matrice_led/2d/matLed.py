14b610b36d92050ec67b83fa554788715939bdac
Michel Osee <mosee@ulb.ac.be>
2019-04-02 14:42:17 +0200

    Correction de bug dans le test de la matrice2D

--- 03_matrice_led/2d/matLed.py
+++ 03_matrice_led/2d/matLed.py
@@ -2,10 +2,10 @@ import i2cComm
 import dicoMatrice2D
 import time
 
-#block = [0,1,1,0,0, 1,0,0,1,0, 1,1,1,1,0, 1,0,0,1,0, 1,0,0,1,0]
 
-for matrice in dicoMatrice2D.dico2D.values():
+for key,matrice in dicoMatrice2D.dico2D.items():
+    print(key)
     for ligne in matrice:
-        for led in ligne
-            i2cComm.writeNumber(data)
-    time.sleep(1)
+        for led in ligne:
+            i2cComm.writeNumber(led)
+    time.sleep(3)
