f71704c96a9d47bfc80c01c5464f993f19bae041
DESKTOP-2035HHQ\mosee <mosee@ulb.ac.be>
2019-04-02 14:11:34 +0200

    modif pour utiliser les dicos et tableaux

--- 03_matrice_led/2d/matLed.py
+++ 03_matrice_led/2d/matLed.py
@@ -4,7 +4,8 @@ import time
 
 #block = [0,1,1,0,0, 1,0,0,1,0, 1,1,1,1,0, 1,0,0,1,0, 1,0,0,1,0]
 
-for element in dicoMatrice2D.dico2D:
-    for data in element:
-        i2cComm.writeNumber(data)
+for matrice in dicoMatrice2D.dico2D.values():
+    for ligne in matrice:
+        for led in ligne
+            i2cComm.writeNumber(data)
     time.sleep(1)
