71c6b6737b0d90fde6345e5f6bb5f292d282f3d0
nicodft <nico1831@hotmail.com>
2019-05-15 16:22:04 +0200

    ajout variables.py

---
+++ 00_algorithmique/01_python/variables.py
@@ -0,0 +1,4 @@
+#il existe 3 types de variables sous python
+variable1 = 5 #integer > int qui symbolise un nombre entier
+variable2 = 3.2 #float > float qui symbolise un chiffre à virgule
+variable3 = "coucou" # string > str qui symbolise du texte
